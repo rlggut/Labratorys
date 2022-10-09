588d594f4196cd197f250c1334b730e2bd5d95b0
rlggut <rlggut@mail.ru>
2022-10-09 19:00:23 +0300

    [Matrix] fix multiple declaration

--- Matrix.py
+++ Matrix.py
@@ -5,26 +5,26 @@ class Matrix:
         for i in range(self.n):
             nillVec.append(0)
         for j in range(self.m):
-            self.matr.extend(nillVec)
-    def __init__(self, n):
-        self.n=n
-        self.m=n
-        self.createNill()
-    def __init__(self, m, n):
+            self.matr.append(nillVec)
+    def __init__(self, m=3, n='!'):
+        if not(isinstance(m, int)):
+            m=3
+        if(n=='!'):
+            if(m<=0):
+                m=3
+            n=m
         self.n=n
         self.m=m
         self.createNill()
-    def __init__(self):
-        self.n=3
-        self.m=3
-        self.createNill()
+
     def __str__(self):
         res=""
-        print(self.matr)
-        for elem in self.matr:
-            res += str(elem)+ " " 
+        for i in range(len(self.matr)):
+            for elem in self.matr[i]:
+                res += str(elem)+ " "
+            res+='\n'
         return res
         
         
-ma=Matrix()
+ma=Matrix(3)
 print(ma)
