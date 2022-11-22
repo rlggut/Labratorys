547341564cea518f6f8d82dab9876e93d23aa4a5
rlggut <rlggut@mail.ru>
2022-11-22 15:20:39 +0300

    [TestMaker] gui update

--- TestMaker/Main.py
+++ TestMaker/Main.py
@@ -11,19 +11,19 @@ class App:
     def __init__(self):
         self.window = Tk()
         self.window.title("Masking picture")
-        size="640x400"
+        size="400x200"
         self.window.geometry(size)
         self.frame = Frame(self.window)
         self.frame.grid()
         self.variant=Variant()
 
         self.lblTypeOfWork = Label(self.frame, text="Choose type of work")
-        self.lblTypeOfWork.grid(column=0, row=0)
-        self.var = IntVar()
-        self.radioClass = Radiobutton(self.frame, text="Classwork", variable=self.var, value=2, command=self.__saveFileDir)
-        self.radioClass.grid(column=1, row=0)
-        self.radioHome = Radiobutton(self.frame, text="Homework", variable=self.var, value=1, command=self.__saveFileDir)
-        self.radioHome.grid(column=2, row=0)
+        self.lblTypeOfWork.grid(column=1, row=0, padx=10)
+        self.varTypeWork = IntVar()
+        self.radioClass = Radiobutton(self.frame, text="Classwork", variable=self.varTypeWork, value=2, command=self.__saveFileDir)
+        self.radioClass.grid(column=2, row=0)
+        self.radioHome = Radiobutton(self.frame, text="Homework", variable=self.varTypeWork, value=1, command=self.__saveFileDir)
+        self.radioHome.grid(column=3, row=0)
         self.radioClass.select()
 
         now = datetime.datetime.now()
@@ -43,54 +43,85 @@ class App:
         self.yearSpin = ttk.Spinbox(self.lblDataSelect , from_=now.year, to_=now.year+10, width=5, command=self.__correctData)
         self.yearSpin.set(now.year)
         self.yearSpin.grid(column=5, row=0)
-        self.lblDataSelect.grid(column=0, row=1, columnspan=5)
+        self.lblDataSelect.grid(column=1, row=1, columnspan=5, padx=10)
 
         self.filenameDir="C:"
         self.lblSave = Label(self.frame, text="Where to save")
-        self.lblSave.grid(column=0, row=2)
+        self.lblSave.grid(column=1, row=2, padx=10)
         self.btnDirSave = Button(self.frame, text="...", command=self.__clickedSaving)
-        self.btnDirSave.grid(column=1, row=2)
+        self.btnDirSave.grid(column=2, row=2)
         self.lblSaveFile = Label(self.frame, text="Will be saved as...")
-        self.lblSaveFile.grid(column=0, row=3, columnspan=7)
+        self.lblSaveFile.grid(column=1, row=3, columnspan=7, padx=10)
         self.__saveFileDir()
 
         self.lblVariant = Label(self.frame, text="Choose key:")
-        self.lblVariant.grid(column=0, row=4)
+        self.lblVariant.grid(column=1, row=4, padx=10)
         self.entryKey = Entry(self.frame)
-        self.entryKey.grid(column=1, row=4)
+        self.entryKey.grid(column=2, row=4)
+        self.btnSaveKey = Button(self.frame, text="Set key", command=self.__SetVariantKey)
+        self.btnSaveKey.grid(column=3, row=4)
+
+        self.lblSaveTypeSelect = LabelFrame(self.frame, text='Create&Save', labelanchor=N)
+        self.varSaveWork = IntVar()
+        self.checkWorkSave = Checkbutton(self.lblSaveTypeSelect, text='Work', variable=self.varSaveWork)
+        self.varSaveWork.set(1)
+        self.checkWorkSave.grid(column=0, row=0)
+        self.varSaveAnswer = IntVar()
+        self.checkAnswerSave = Checkbutton(self.lblSaveTypeSelect, text='Answer', variable=self.varSaveAnswer)
+        self.varSaveAnswer.set(1)
+        self.checkAnswerSave.grid(column=1, row=0)
+        self.lblSaveTypeSelect.grid(column=1, row=5, columnspan=1, padx=10)
+
         self.btnSave = Button(self.frame, text="Create&Save", command=self.__SavingVariant)
-        self.btnSave.grid(column=2, row=4)
+        self.btnSave.grid(column=2, row=5)
 
         self.window.mainloop()
-
+    def __SetVariantKey(self):
+        keyNum=0
+        for ch in self.entryKey.get():
+            if(ch<'0'):
+                ch='0'
+            keyNum=(keyNum*10+ord(ch)-ord('0'))
+            keyNum=keyNum % 1000000
+        print(keyNum)
+        self.variant.setVariantNum(keyNum)
     def __SavingVariant(self):
-        fopen = open(self.filenameDir+"/"+self.filenameSave, 'w')
+        #self.__SetVariantKey()
         footer=""
-        if(self.var.get()==1):
+        if(self.varTypeWork.get()==1):
             footer = "Homework"
         else:
             footer = "Classwork"
         footer = footer+"              "+self.daySpin.get()+"/"+self.monthSpin.get()+"/"+str(int(self.yearSpin.get())%100)
-        fopen.write(footer+"\n")
-        fopen.write(self.variant.getQuestion(1))
-        fopen.close()
+
+        if(self.varSaveWork.get()):
+            fopen = open(self.filenameDir+"/"+self.filenameSave, 'w')
+            fopen.write(footer+"\n")
+            fopen.write(self.variant.getCategoriesQuestions())
+            fopen.close()
 
         asnswFileName = self.filenameSave
         asnswFileName = asnswFileName.rstrip('.doc')
         asnswFileName = asnswFileName+"_answer.doc"
-        fopen = open(self.filenameDir+"/"+asnswFileName, 'w')
-        fopen.write(footer+"\n")
-        fopen.write(self.variant.getAnswer(1))
-        fopen.close()
+        if(self.varSaveAnswer.get()):
+            fopen = open(self.filenameDir+"/"+asnswFileName, 'w')
+            fopen.write(footer+"\n")
+            fopen.write(self.variant.getCategoriesQuestions())
+            fopen.close()
 
     def __saveFileDir(self):
         self.filenameSave=""
-        if(self.var.get()==1):
+        if(self.varTypeWork.get()==1):
             self.filenameSave = "Homework "
         else:
             self.filenameSave = "Classwork "
         self.filenameSave = self.filenameSave+"("+self.daySpin.get()+"_"+self.monthSpin.get()+"_"+self.yearSpin.get() + ").doc"
-        self.lblSaveFile["text"]="Will be saved as: "+self.filenameDir +"/"+ self.filenameSave
+        place=self.filenameDir +"/"+ self.filenameSave
+        countMax=45
+        if(len(place)>countMax):
+            place = self.filenameDir[:(countMax-len(self.filenameSave))]
+            place = place + ".../" + self.filenameSave
+        self.lblSaveFile["text"]="Will be saved as: "+place
 
     def __correctData(self):
         days = monthrange(int(self.yearSpin.get()), int(self.monthSpin.get()))[1]
