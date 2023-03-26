cd483ba680097843dd271b08d66f4c3db4651ad8
rlggut <rlggut@mail.ru>
2023-03-26 12:49:39 +0300

    [Sound analize] Добавлен алгоритм удаление тишини из записи

--- Sound analize/main (2).py
+++ Sound analize/main (2).py
@@ -17,14 +17,16 @@ class App:
     def __init__(self):
         self.window = Tk()
         self.window.title("Осцилограмма")
-        self.canvasW = 800
+        self.canvasW = 1000
         self.canvasH = 400
-        self.window.geometry("800x400")
+        self.researchTime = 3
+        self.researchDelta=100
+        self.window.geometry("1000x400")
         self.frame = Frame(self.window)
         self.frame.grid()
 
         self.filename = "sample.wav"
-        self.__getInfo()
+        self.__getData()
 
         self.canvas = Canvas(self.frame, height=self.canvasH, width=self.canvasW, bg='white')
         self.canvas.grid(column=0, row=0, rowspan=12)
@@ -32,24 +34,42 @@ class App:
         self.__Draw()
         self.window.mainloop()
 
-    def __getInfo(self):
+    def __getData(self):
         self.wav = wave.open(self.filename, mode="r")
         (self.nchannels, self.sampwidth, self.framerate, self.nframes, comptype, compname) = self.wav.getparams()
-        print(self.nchannels, self.sampwidth, self.framerate, self.nframes)
-
+        self.content = self.wav.readframes(min(self.researchTime * self.framerate, self.nframes))
+        self.samples = pointFromBuff(self.content, self.sampwidth)
+        self.__deleteSilence()
+    def __deleteSilence(self):
+        pointForResearch = self.researchDelta * self.framerate // 1000
+        newSample=[]
+        for i in range(0,len(self.samples)-pointForResearch,pointForResearch):
+            power=0
+            for j in range(pointForResearch):
+                power+=abs(self.samples[i+j])
+            power /= pointForResearch
+            if(power>4*self.researchDelta):
+                for j in range(pointForResearch):
+                    newSample.append(self.samples[i+j])
+        self.samples = newSample
     def __Draw(self):
         sizeY=1024
         step=1
-        content = self.wav.readframes(min(3*self.framerate,self.nframes))
-        samples = pointFromBuff(content, self.sampwidth)
-        image1 = Image.new("RGB", (len(samples)//step, sizeY), "white")
+
+        image1 = Image.new("RGB", (len(self.samples)//step, sizeY), "white")
         draw = ImageDraw.Draw(image1)
+
+        draw.line([(0, sizeY//2), (len(self.samples)//step, sizeY // 2)], fill="green", width=1)
+        ind=1
+        while(ind*self.framerate<len(self.samples)):
+            draw.line([(ind*self.framerate, sizeY-sizeY//8), (ind*self.framerate, sizeY)], fill="blue", width=16)
+            ind += 1
+
         maxAmp=2**(self.sampwidth*8-1)
         lastY=sizeY//2
         t=1
-        for i in range(0,len(samples),step):
-            posY = samples[i]
-            #print(samples[i],content[2*i],content[2*i+1])
+        for i in range(0,len(self.samples),step):
+            posY = self.samples[i]
             posY = (posY*(sizeY//2))//maxAmp
             draw.line([(t-1, lastY), (t, -posY + sizeY//2)], fill =128,width=1)
             lastY = -posY + (sizeY//2)
@@ -58,4 +78,5 @@ class App:
         self.photo = ImageTk.PhotoImage(self.image)
         self.с_image = self.canvas.create_image(0, 0, anchor='nw', image=self.photo)
         image1.save("res.png")
+
 app=App()
