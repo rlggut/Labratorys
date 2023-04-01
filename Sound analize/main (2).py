9a036d4c6c83520add452bfb5e94f1b68a7d92ca
rlggut <rlggut@mail.ru>
2023-04-02 02:06:44 +0300

    [Sound analize] Пробная версия спектрограммы

--- Sound analize/main (2).py
+++ Sound analize/main (2).py
@@ -2,6 +2,7 @@ from tkinter import *
 import wave
 from SignalWidget import *
 from PIL import Image, ImageDraw, ImageTk
+from SpectrForm import *
 
 def pointFromBuff(buff, sampwidth):
     points=[]
@@ -18,19 +19,22 @@ class App:
     def __init__(self):
         self.window = Tk()
         self.window.title("Осцилограмма")
-        self.canvasW = 1000
-        self.canvasH = 400
+        self.canvasW = 600
+        self.canvasH = 200
         self.researchTime = 3
         self.researchDelta=100
-        self.window.geometry("1000x450")
+        self.window.geometry("600x450")
         self.frame = Frame(self.window)
         self.frame.grid()
 
         self.filename = "sample.wav"
         self.__getData()
 
-        self.canvas = Canvas(self.frame, height=self.canvasH, width=self.canvasW, bg='white')
-        self.canvas.grid(column=0, row=0, columnspan=5)
+        self.canvasOscill = Canvas(self.frame, height=self.canvasH, width=self.canvasW, bg='white')
+        self.canvasOscill.grid(column=0, row=0, columnspan=5)
+
+        self.canvasSpectr = Canvas(self.frame, height=self.canvasH, width=self.canvasW, bg='white')
+        self.canvasSpectr.grid(column=0, row=2, columnspan=5)
 
         self.btnLeft = Button(self.frame, text="<-", command=self.__moveOscillLeft, state=DISABLED)
         self.btnLeft.grid(column=0, row=1)
@@ -53,12 +57,12 @@ class App:
         self._frameImage = (self._frameImage*2)
         if(self._frameImage+self._from>self._numsOfFrames):
             self._from = self._numsOfFrames - self._frameImage
-        self._wgt.setZoom(self._frameImage)
+        self._signalWgt.setZoom(self._frameImage)
         self.__correctButtons()
         self.__Draw()
     def __zoomIn(self):
         self._frameImage=max(1,(self._frameImage//2))
-        self._wgt.setZoom(self._frameImage)
+        self._signalWgt.setZoom(self._frameImage)
         self.__correctButtons()
         self.__Draw()
     def __correctButtons(self):
@@ -78,17 +82,14 @@ class App:
             self.btnLeft['state'] = 'disabled'
         else:
             self.btnLeft['state'] = 'normal'
-
     def __moveOscillLeft(self):
         self._from=max(self._from-1,0)
         self.__correctButtons()
         self.__Draw()
-
     def __moveOscillRight(self):
         self._from = min(self._from + 1, self._numsOfFrames-self._frameImage)
         self.__correctButtons()
         self.__Draw()
-
     def __getData(self):
         self.wav = wave.open(self.filename, mode="r")
         (self._nchannels, self.sampwidth, self._framerate, self._nframes, comptype, compname) = self.wav.getparams()
@@ -96,14 +97,17 @@ class App:
         self.samples = pointFromBuff(self.content, self.sampwidth)
         self.__deleteSilence()
 
-        self._timePerImage = 100
+        self._timePerImage = 50
         self._frameImage = 4
         self._sizeForFrame = ((self._timePerImage * self._framerate) // 1000)
         self._time = (len(self.samples)*1000) // self._framerate
         self._numsOfFrames = math.ceil(self._time // self._timePerImage)
-        self._wgt = signalwidget(self._framerate, self.sampwidth, 1024, self._timePerImage, self._frameImage)
-        self._wgt.setData(self.samples)
-        self._from=0
+        self._signalWgt = signalwidget(self._framerate, self.sampwidth, 1024, self._timePerImage, self._frameImage)
+        self._signalWgt.setData(self.samples)
+        self._from = 0
+
+        self._spectr = spectrofm(1024, 1)
+
 
     def __deleteSilence(self):
         pointForResearch = self.researchDelta * self._framerate // 1000
@@ -118,10 +122,18 @@ class App:
                     newSample.append(self.samples[i+j])
         self.samples = newSample
     def __Draw(self):
-        self.image = self._wgt.getImage(self._from)
-        self.image =self.image .resize((self.canvasW, self.canvasH))
-        self.photo = ImageTk.PhotoImage(self.image)
-        self.с_image = self.canvas.create_image(0, 0, anchor='nw', image=self.photo)
+        self._spectrData = FFTAnalysis(self.samples[(self._from)*self._sizeForFrame:
+                                                    (self._from+self._frameImage)*self._sizeForFrame])
+        self._spectr.setData(self._spectrData)
+        self._spectr.makeZone([0.1, 0.4, 0.3, 0.2])
+
+        self.imageOscill = self._signalWgt.getImage(self._from).resize((self.canvasW, self.canvasH))
+        self.imageSpectr = self._spectr.getImage().resize((self.canvasW, self.canvasH))
+        self.photoOscill = ImageTk.PhotoImage(self.imageOscill)
+        self.photoSpectr = ImageTk.PhotoImage(self.imageSpectr)
+        self.с_imageOscl = self.canvasOscill.create_image(0, 0, anchor='nw', image=self.photoOscill)
+
+        self.с_imageSpctr = self.canvasSpectr.create_image(0, 0, anchor='nw', image=self.photoSpectr)
 
         tStart = self._from * self._timePerImage
         timeStr = str(tStart//1000)+":"
