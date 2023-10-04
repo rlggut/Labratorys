438166c008bc12fce540629c6b29d18081a08cb9
rlggut <rlggut@mail.ru>
2023-10-04 22:02:17 +0300

    [MatrixTraining] Предварительное преобразование изображений в градации серого. Учет направления по оси Y

--- MatrixTraining/main.py
+++ MatrixTraining/main.py
@@ -1,6 +1,6 @@
 #License: CC BY
 #Roman Gutenkov, 28/05/23
-#Version: 0.2.1
+#Version: 0.2.2
 
 from tkinter import *
 
@@ -47,6 +47,7 @@ class imageAnalizer():
             if(newFileName==""):
                 if(self._fileName==""): self._fileName="newPic.png"
             else: self._fileName=newFileName
+        #Подсчет для направления по оси X
         matrUD=Matrix(3,3)
         matrUD.setData([[-1,0,0],
                         [0,0,0],
@@ -59,8 +60,27 @@ class imageAnalizer():
         matrDU.setData([[0,0,1],
                         [0,0,0],
                         [-1,0,0]])
-        tmp=countMatrixGrad(self._image,[matrUD,matrL,matrDU],self._scale )
-        print('Кол-во совпадений:',tmp)
+        self._image = self._image.convert("L")
+        tmpX=countMatrixGrad(self._image,[matrUD,matrL,matrDU],self._scale)
+        print('Кол-во совпадений векторов градиента по X: ', tmpX)
+
+        # Подсчет для направления по оси Y
+        matrU=Matrix(3,3)
+        matrU.setData([[0,-1,0],
+                       [0,0,0],
+                       [0,1,0]])
+        matrRL=Matrix(3,3)
+        matrRL.setData([[0,0,-1],
+                        [0,0,0],
+                        [1,0,0]])
+        self._image = self._image.convert("L")
+        tmpY=countMatrixGrad(self._image,[matrUD,matrU,matrRL],self._scale)
+        print('Кол-во совпадений векторов градиента по Y: ',tmpY)
+        # Усреднение, чтобы матрицы были эквивалентными
+        tmp = []
+        for i in range(3):
+            tmp.append((tmpX[i] + tmpY[i]) / 2)
+
         mx=tmp.index(max(tmp))
         mn=tmp.index(min(tmp))
         if(mn == mx): mn = 2
@@ -69,23 +89,23 @@ class imageAnalizer():
         if(tmp[mid]-tmp[mn]<0.1*tmp[mid]): tmp[mn]=tmp[mid]
         res=[1,1,1]
         if(tmp[mn]>0):
-            res[mid]=math.ceil(tmp[mid]/tmp[mn])
+            res[mid]=math.ceil(tmp[mid] / tmp[mn])
             res[mx] = math.ceil(tmp[mx] / tmp[mn])
         else:
             res[mid]=0
             res[mx]=0
             res[mn]=0
-        addMatr = self._matr.getRate() * (res[0] * matrUD + res[1] * matrL + res[2] * matrDU)
-        addMatr.normalizedX()
-        #print(addMatr)
-        #print("+")
-        #print(self._matr._matrX)
-        self._matr._matrX = self._matr._matrX + addMatr
-        #print("=")
-        #print(self._matr._matrX)
+
+        addMatrX = self._matr.getRate() * (res[0] * matrUD + res[1] * matrL + res[2] * matrDU)
+        addMatrX.normalizedX()
+        addMatrY = self._matr.getRate() * (res[0] * matrUD + res[1] * matrU + res[2] * matrRL)
+        addMatrY.normalizedY()
+
+        self._matr._matrX = self._matr._matrX + addMatrX
+        self._matr._matrY = self._matr._matrY + addMatrY
         self._matr.cutRate()
     def getMatrix(self):
-        print('Расчетная матрица:')
+        print('Расчетные матрицы:')
         print(self._matr)
 
 
