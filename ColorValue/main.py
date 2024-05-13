c46c9126a78036f6971112b29035f731c7c18586
rlggut <rlggut@mail.ru>
2024-05-13 14:57:00 +0300

    [ColorValue] Рефакторинг и механизм выбора акцентного цвета

--- ColorValue/main.py
+++ ColorValue/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import colorchooser
 from PIL import Image, ImageTk
 import sys
 import os
@@ -29,12 +30,30 @@ class App:
         self.lblChoosed.grid(column=4, row=0, columnspan=3)
         self.filename = ""
 
-        self.canvas = Canvas(self.window, height=2.1*self.canvasH, width=3 * self.canvasW+3)
+        self.btnColor = Button(self.frame, text="Выбор основного цвета", command=self.__clickedColor)
+        self.btnColor.grid(column=7, row=0)
+        self.btnColor["state"] = "disabled"
+        self.color = (40, 66, 57)
+        self.canvasColor = Canvas(self.window, height=15, width=45)
+        self.imageColor = Image.new('RGB', (self.canvasColor.winfo_width(), self.canvasColor.winfo_height()), self.color)
+        self.photoColor = ImageTk.PhotoImage(self.imageColor)
+        self.с_imageColor = self.canvasColor.create_image(0, 0, anchor='nw', image=self.photoColor)
+        self.canvasColor.grid(column=1, row=0, columnspan=3)
+
+        self.canvas = Canvas(self.window, height=2.1 * self.canvasH, width=3 * self.canvasW + 3)
         self.с_image = self.canvas.create_image(0, 0, anchor='nw', image=self.photo)
-        self.canvas.grid(column=0, row=1,columnspan=5)
+        self.canvas.grid(column=0, row=1,columnspan=8)
 
         self.window.mainloop()
 
+    def __clickedColor(self):
+        (rgb, hx) = colorchooser.askcolor()
+        self.color = rgb
+        self.imageColor = Image.new('RGB', (self.canvasColor.winfo_width(), self.canvasColor.winfo_height()), self.color)
+        self.photoColor = ImageTk.PhotoImage(self.imageColor)
+        self.с_imageColor = self.canvasColor.create_image(0, 0, anchor='nw', image=self.photoColor)
+
+        self.__createExpImages()
     def __clickedLoading(self):
         file = filedialog.askopenfilename(filetypes=[("Image (png/jpg)", ("*.png", "*.jpg"))])
         if(file==""):
@@ -60,6 +79,9 @@ class App:
         self.photoGreyBound = ImageTk.PhotoImage(self.imageGreyBound)
         self.с_imageGreyBound = self.canvas.create_image(self.canvasW+1, self.canvasH+1, anchor='nw', image=self.photoGreyBound)
 
+        self.btnColor["state"] = "normal"
+        self.__createExpImages()
+    def __createExpImages(self):
         self.imageGreyExp = self.__getGreyOwn()
         self.photoGreyExp = ImageTk.PhotoImage(self.imageGreyExp)
         self.с_imageGreyExp = self.canvas.create_image(2*self.canvasW+2, 0, anchor='nw', image=self.photoGreyExp)
@@ -67,14 +89,14 @@ class App:
         self.imageGreyExpBound=maskedImageMatrix(self.imageGreyExp)
         self.photoGreyExpBound = ImageTk.PhotoImage(self.imageGreyExpBound)
         self.с_imageGreyExpBound = self.canvas.create_image(2*self.canvasW+2, self.canvasH+1, anchor='nw', image=self.photoGreyExpBound)
-        #print(compareImageProc(self.imageGreyExpBound,self.imageGreyBound))
+
 
     def __getGrey(self):
         res = self.image.convert("L")
         return res
     def __FsRGB(self, value):
         return int(value[0]*0.2126+value[1]*0.7152+value[2]*0.0722)
-    def __funcGreen(self, value, color=[89,142,86]):
+    def __funcMainColor(self, value, color):
         m1=max(color[0],255-color[0])
         m2=max(color[1],255-color[1])
         m3=max(color[2],255-color[2])
@@ -85,7 +107,7 @@ class App:
         for y in range(self.image.height):
             for x in range(self.image.width):
                 value = self.image.getpixel((x,y))
-                greyCol = self.__funcGreen(value)
+                greyCol = self.__funcMainColor(value, self.color)
                 res.putpixel((x,y), (greyCol,greyCol,greyCol))
         return res
 
