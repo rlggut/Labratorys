02c506cf545deb61625ccd441b3bb9b9b800f517
rlggut <rlggut@mail.ru>
2025-01-09 01:16:39 +0300

    [MP3Tagger] Добавлен перечень жанров с автозаполнением

--- MP3Tagger/Main.py
+++ MP3Tagger/Main.py
@@ -4,8 +4,11 @@
 
 from tkinter import *
 from tkinter import filedialog
+from ttkwidgets.autocomplete import AutocompleteCombobox
 import sys
 import os
+from tkinter.ttk import Combobox
+
 import eyed3
 #Жанры
 #https://en.wikipedia.org/wiki/List_of_ID3v1_genres
@@ -18,6 +21,199 @@ class App:
         self.frame = Frame(self.window)
         self.frame.grid()
         self.path=""
+
+        genre = ["Blues",
+            "Classic Rock",
+            "Country",
+            "Dance",
+            "Disco",
+            "Funk",
+            "Grunge",
+            "Hip-Hop",
+            "Jazz",
+            "Metal",
+            "New Age",
+            "Oldies",
+            "Other",
+            "Pop",
+            "R&B",
+            "Rap",
+            "Reggae",
+            "Rock",
+            "Techno",
+            "Industrial",
+            "Alternative",
+            "Ska",
+            "Death Metal",
+            "Pranks",
+            "Soundtrack",
+            "Euro-Techno",
+            "Ambient",
+            "Trip-Hop",
+            "Vocal",
+            "Jazz & Funk",
+            "Fusion",
+            "Trance",
+            "Classical",
+            "Instrumental",
+            "Acid",
+            "House",
+            "Game",
+            "Sound Clip",
+            "Gospel",
+            "Noise",
+            "Alternative Rock",
+            "Bass",
+            "Soul",
+            "Punk",
+            "Space",
+            "Meditative",
+            "Instrumental Pop",
+            "Instrumental Rock",
+            "Ethnic",
+            "Gothic",
+            "Darkwave",
+            "Techno-Industrial",
+            "Electronic",
+            "Pop-Folk",
+            "Eurodance",
+            "Dream",
+            "Southern Rock",
+            "Comedy",
+            "Cult",
+            "Gangsta",
+            "Top 40",
+            "Christian Rap",
+            "Pop/Funk",
+            "Jungle",
+            "Native US",
+            "Cabaret",
+            "New Wave",
+            "Psychadelic",
+            "Rave",
+            "Showtunes",
+            "Trailer",
+            "Lo-Fi",
+            "Tribal",
+            "Acid Punk",
+            "Acid Jazz",
+            "Polka",
+            "Retro",
+            "Musical",
+            "Rock 'n' Roll",
+            "Hard Rock",
+            "Folk",
+            "Folk-Rock",
+            "National Folk",
+            "Swing",
+            "Fast Fusion",
+            "Bebop",
+            "Latin",
+            "Revival",
+            "Celtic",
+            "Bluegrass",
+            "Avantgarde",
+            "Gothic Rock",
+            "Progressive Rock",
+            "Psychedelic Rock",
+            "Symphonic Rock",
+            "Slow Rock",
+            "Big Band",
+            "Chorus",
+            "Easy Listening",
+            "Acoustic",
+            "Humour",
+            "Speech",
+            "Chanson",
+            "Opera",
+            "Chamber Music",
+            "Sonata",
+            "Symphony",
+            "Booty Bass",
+            "Primus",
+            "Porn Groove",
+            "Satire",
+            "Slow Jam",
+            "Club",
+            "Tango",
+            "Samba",
+            "Folklore",
+            "Ballad",
+            "Power Ballad",
+            "Rhythmic Soul",
+            "Freestyle",
+            "Duet",
+            "Punk Rock",
+            "Drum Solo",
+            "A capella",
+            "Euro-House",
+            "Dance Hall",
+            "Goa",
+            "Drum & Bass",
+            "Club-House",
+            "Hardcore Techno",
+            "Terror",
+            "Indie",
+            "BritPop",
+            "Negerpunk",
+            "Polsk Punk",
+            "Beat",
+            "Christian Gangsta Rap",
+            "Heavy Metal",
+            "Black Metal",
+            "Crossover",
+            "Contemporary Christian",
+            "Christian Rock",
+            "Merengue",
+            "Salsa",
+            "Thrash Metal",
+            "Anime",
+            "Jpop",
+            "Synthpop",
+            "Abstract",
+            "Art Rock",
+            "Baroque",
+            "Bhangra",
+            "Big Beat",
+            "Breakbeat",
+            "Chillout",
+            "Downtempo",
+            "Dub",
+            "EBM",
+            "Eclectic",
+            "Electro",
+            "Electroclash",
+            "Emo",
+            "Experimental",
+            "Garage",
+            "Global",
+            "IDM",
+            "Illbient",
+            "Industro-Goth",
+            "Jam Band",
+            "Krautrock",
+            "Leftfield",
+            "Lounge",
+            "Math Rock",
+            "New Romantic",
+            "Nu-Breakz",
+            "Post-Punk",
+            "Post-Rock",
+            "Psytrance",
+            "Shoegaze",
+            "Space Rock",
+            "Trop Rock",
+            "World Music",
+            "Neoclassical",
+            "Audiobook",
+            "Audio Theatre",
+            "Neue Deutsche Welle",
+            "Podcast",
+            "Indie Rock",
+            "G-Funk",
+            "Dubstep",
+            "Garage Rock",
+            "Psybien"]
 
         self.btnFileChoose = Button(self.frame, text="Выбрать .mp3", command=self.__open)
         self.btnFileChoose.grid(column=0, row=0)
@@ -46,8 +242,8 @@ class App:
 
         self.lblGenre = Label(self.frame, text="Жанр")
         self.lblGenre.grid(column=0, row=5, padx=10)
-        self.entryGenre = Entry(self.frame, text="", state=DISABLED, validatecommand=self.__anyChange)
-        self.entryGenre.grid(column=1, row=5, padx=10)
+        self.comboGenre = AutocompleteCombobox(self.frame, completevalues=genre, state=DISABLED, validatecommand=self.__anyChange)
+        self.comboGenre.grid(column=1, row=5, padx=10)
 
         self.window.mainloop()
 
@@ -62,7 +258,7 @@ class App:
             self.entryAlbum["state"]    = "normal"
             self.entryName["state"]     = "normal"
             self.spinNum["state"]       = "normal"
-            self.entryGenre["state"]    = "normal"
+            self.comboGenre["state"]    = "normal"
             self.btnFileSave["state"]   = "normal"
             self.btnFileSave['text']    = "Сохранить метаданные"
 
@@ -70,13 +266,13 @@ class App:
             self.entryAlbum.delete(0,END)
             self.entryName.delete(0,END)
             self.spinNum.delete(0,END)
-            self.entryGenre.delete(0,END)
+            self.comboGenre.delete(0,END)
 
             self.entryArtist.insert(0,audioFile.tag.artist)
             self.entryAlbum.insert(0,audioFile.tag.album)
             self.entryName.insert(0,audioFile.tag.title)
             self.spinNum.insert(0,audioFile.tag.track_num[0])
-            self.entryGenre.insert(0,audioFile.tag.genre)
+            self.comboGenre.insert(0,audioFile.tag.genre)
     def __save(self):
             audioFile = eyed3.load(self.path)
             audioFile.tag.artist = self.entryArtist.get()
@@ -84,7 +280,7 @@ class App:
             audioFile.tag.album_artist = audioFile.tag.artist
             audioFile.tag.title = self.entryName.get()
             audioFile.tag.track_num = self.spinNum.get()
-            audioFile.tag.genre = self.entryGenre.get()
+            audioFile.tag.genre = self.comboGenre.get()
             audioFile.tag.save()
 
             self.btnFileSave['text']    = "Сохранено"
@@ -93,6 +289,6 @@ class App:
             self.entryAlbum["state"]    = "disabled"
             self.entryName["state"]     = "disabled"
             self.spinNum["state"]       = "disabled"
-            self.entryGenre["state"]    = "disabled"
+            self.comboGenre["state"]    = "disabled"
             self.btnFileSave["state"]   = "disabled"
 app=App()
